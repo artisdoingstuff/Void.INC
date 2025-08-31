a26c68d1f7b224422467910f5932da842109e961
Scott <platedoesstuff@gmail.com>
2025-08-31 19:08:06 +0800

    Update .gitignore

--- Incremental/Marketplace/Marketplace.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Misc/Includes.hpp"
-
-// Empty for now, will be worked on for 1.4.3
--- Incremental/Marketplace/PotionBrewery.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-// why the hell is this feature broken
-// like, it just doesn't work
-// no idea why
-// send help
-// will fix and add in 1.4.x
